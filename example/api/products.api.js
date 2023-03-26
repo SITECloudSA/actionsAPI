7de39cd4746a7e9280de2eeae1930cad179ab5e6
Mohammad <malhareeqi@site.sa>
2023-03-26 12:59:04 +0000

    MVP

--- example/api/products.api.js
+++ example/api/products.api.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const { apiRouter, QUERY } = require("actionsAPI");
-=======
-const { apiRouter, QUERY } = require("../../index");
->>>>>>> 5f636922230ca29378c0001b0035768c0df2059c
 const cont = require("../controller");
 
 const app = apiRouter();
--- example/api/user.api.js
+++ example/api/user.api.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 const { apiRouter, PARAM } = require("actionsAPI");
-=======
-const { apiRouter, PARAM } = require("../../index");
->>>>>>> 5f636922230ca29378c0001b0035768c0df2059c
 const cont = require("../controller");
 
 const app = apiRouter();
