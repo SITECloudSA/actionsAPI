921124573e5144c6e937ff91ff4b0a999b2dd4a7
Mohammad <malhareeqi@site.sa>
2023-03-26 13:01:31 +0000

    MVP

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 module.exports = require("express");
 module.exports.apiRouter = require("./lib/router").apiRouter;
-<<<<<<< HEAD
 module.exports.ApiRouter = require("./lib/router").ApiRouter;
-=======
-module.exports.apiRouterSetErrorHandler = require("./lib/router").apiRouterSetErrorHandler;
-module.exports.apiRouterSetNotFound = require("./lib/router").apiRouterSetNotFound;
->>>>>>> 5f636922230ca29378c0001b0035768c0df2059c
 module.exports.PARAM = "PARAM";
 module.exports.QUERY = "QUERY";
 module.exports.BODY = "BODY";
