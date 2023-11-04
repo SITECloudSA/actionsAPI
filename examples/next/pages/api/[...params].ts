17183d3cace86a8159aac5faeb3d835be28b3ca2
Ezziddin <alaa.ezziddin.aae@gmail.com>
2023-11-04 19:47:44 +0300

    prepare dev env for next

--- examples/next/pages/api/[...params].ts
+++ examples/next/pages/api/[...params].ts
@@ -1,8 +1,3 @@
-import { NextResponse } from "next/server";
-
-// export async function GET() {
-//   return NextResponse.json("here");
-// }
 export default function handler(req: any, res: any) {
   res.status(200).json("hi");
 }
