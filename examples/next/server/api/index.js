d441d15817fb445871fcdfb883cee10f2b0f9e16
c.aezzidin <c.aezzidin@site.sa>
2023-11-30 12:50:01 +0000

    sdkGenerator

--- examples/next/server/api/index.js
+++ examples/next/server/api/index.js
@@ -4,7 +4,7 @@ const apiRouter = apiApp();
 const app = apiApp();
 
 apiRouter.PreAction(({ action, input, route, req, res, context }) => {
-  console.log(`You are about to excute an action called`, action.name);
+  console.log(`You are about to excute ann action called`, action.name);
   return input;
 });
 
@@ -12,10 +12,9 @@ apiRouter.use("/users", require("./user.api"));
 apiRouter.use("/products", require("./products.api"));
 apiRouter.use("/docs", apiRouter.generateDocs());
 
-apiRouter.generateSDK();
-
 app.use("/api", apiRouter);
 
+app.generateSDK();
 app.use("*", app.notFoundRequest);
 
 module.exports = app;
