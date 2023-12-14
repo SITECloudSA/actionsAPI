30d4f6f3a27048d0d917d456cbd797cbe5206db3
c.aezzidin <c.aezzidin@site.sa>
2023-12-14 12:16:07 +0000

    useSdk wip

--- examples/next/app/page.tsx
+++ examples/next/app/page.tsx
@@ -2,13 +2,16 @@
 
 import { useEffect } from "react";
 import styles from "./page.module.css";
-import { SDK, setSdkConfig } from "../sdk.gen";
+import { SDK, useSdk } from "../sdk.gen";
 
 export default function Home() {
   const fetch = async () => {
     const users = await SDK.getAllUsers();
     console.log({ users });
   };
+
+  const { data, loading } = useSdk.getAllUsers();
+  console.log({ data, loading });
 
   useEffect(() => {
     fetch();
--- lib/type.d.ts
+++ lib/type.d.ts
@@ -1,10 +1,16 @@
 export interface SdkConfig {
   baseUrl?: string;
   useSockets?: boolean;
-  onStart?({ action, method, path, input, body, config }): void;
-  onSuccess?({ data, response, action, method, path, input, body }): void;
-  onComplete?({ data, response, error, action, method, path, input, body }): void;
-  onFail?({ error, action, method, path, input, body }): void;
+  onStart?({ action, method, path, input, body, isHook, config }): void;
+  onSuccess?({ data, response, action, method, path, input, body, isHook }): void;
+  onComplete?({ data, response, error, action, method, path, input, body, isHook }): void;
+  onFail?({ error, action, method, path, input, body, isHook }): void;
+  interval?: number;
+  indexdbPrefix?: string;
+  overrideInterval?: { [action: string]: number };
+  authorization?: string;
+  headers?: any;
+  contentType?: string;
 }
 
 export function SetSdkConfig(config: SdkConfig): void;
