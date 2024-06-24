4e2bfe9bbb5a620c26fed9fb1143842a78e4b1c0
c.aezzidin <c.aezzidin@site.sa>
2024-06-24 11:30:17 +0000

    bug fix no connecting to wss

--- examples/next/app/page.tsx
+++ examples/next/app/page.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import styles from "./page.module.css";
 import { SDK, useSdk, setSdkConfig } from "../sdk.gen";
 
-setSdkConfig({ indexdbPrefix: "testActionApi", onComplete: (e) => console.log({ e }) });
+setSdkConfig({ indexdbPrefix: "testActionApi", onComplete: (e) => console.log("completed") });
 
 export default function Home() {
   const fetch = async () => {
